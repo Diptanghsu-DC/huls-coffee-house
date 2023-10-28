a47ad537033230e73a2ee8d8c64060b1d50e917a
Arunaditya Dasgupta <adg8503@gmail.com>
2023-10-28 21:38:48 +0530

    signup page created

--- lib/pages/login_ui/widgets/buttons.dart
+++ lib/pages/login_ui/widgets/buttons.dart
@@ -59,4 +59,24 @@ class GoogleButton extends StatelessWidget {
   }
 }
 
+class GoBackButton extends StatelessWidget {
+  const GoBackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+      ),
+      child: IconButton(onPressed: () {
+        Navigator.maybePop(context);
+      }, icon: const Icon(Icons.chevron_left)),
+    );
+  }
+}
+
+
 
