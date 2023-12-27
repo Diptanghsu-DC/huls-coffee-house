45f3b6ac0e0eeddc1e3b8266246a0181ae3e6e44
Arunaditya Dasgupta <adg8503@gmail.com>
2023-12-27 22:47:32 +0530

    UI adjustments

--- lib/pages/cart_ui/widgets/cart_appbar.dart
+++ lib/pages/cart_ui/widgets/cart_appbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:huls_coffee_house/pages/cart_ui/utils/colors.dart';
 import 'package:huls_coffee_house/pages/cart_ui/utils/styles.dart';
+import 'package:huls_coffee_house/pages/login_ui/widgets/buttons.dart';
 
 class CartAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CartAppBar({super.key});
@@ -12,14 +13,7 @@ class CartAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      elevation: 4,
-      backgroundColor: AppColors.whiteColor,
-      leading: IconButton(
-        icon: const Icon(Icons.arrow_back, color: AppColors.blackColor),
-        onPressed: () {
-          // Implement navigation to previous page here
-        },
-      ),
+      leading: GoBackButton(),
       title: const Text(
         'Cart',
         style: AppStyles.headingStyle,
