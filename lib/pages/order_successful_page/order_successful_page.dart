c7c58b6c17a2ea4c753c7ce981d7147734e860ad
Arunaditya Dasgupta <adg8503@gmail.com>
2024-01-10 20:34:03 +0530

    animation done

--- lib/pages/order_successful_page/order_successful_page.dart
+++ lib/pages/order_successful_page/order_successful_page.dart
@@ -8,23 +8,59 @@ import 'package:huls_coffee_house/pages/homepage_ui/homepage.dart';
 class OrderSuccessfulPage extends StatefulWidget {
   static const String routeName = '/orderSuccessfulPage';
 
-  const OrderSuccessfulPage({super.key});
+  const OrderSuccessfulPage({Key? key}) : super(key: key);
 
   @override
   State<OrderSuccessfulPage> createState() => _OrderSuccessfulPageState();
 }
 
-class _OrderSuccessfulPageState extends State<OrderSuccessfulPage> {
+class _OrderSuccessfulPageState extends State<OrderSuccessfulPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  late Animation<Offset> _slideAnimation;
+
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(-1.0, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+
     if (UserController.currentUser != null) {
-      Timer(Duration(seconds: 5), () {
+      _animationController.forward();
+      Timer(Duration(seconds: 4), () {
         Navigator.pushNamedAndRemoveUntil(
-            context, Homepage.routeName, (route) => false);
+          context,
+          Homepage.routeName,
+              (route) => false,
+        );
       });
     }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -36,41 +72,68 @@ class _OrderSuccessfulPageState extends State<OrderSuccessfulPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Yayy!",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'SofiaPro',
-                  fontSize: 30,
-                  fontStyle: FontStyle.italic),
-            ),
-            Text(
-              "Order Placed Successfully",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'SofiaPro',
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(shape: BoxShape.circle, color: orange),
-              child: Center(
-                child: Icon(
-                  Icons.done_rounded,
-                  color: Colors.white,
-                  size: 40,
+            SlideTransition(
+              position: _slideAnimation,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  "Yayy!",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'SofiaPro',
+                    fontSize: 30,
+                    fontStyle: FontStyle.italic,
+                  ),
                 ),
               ),
             ),
+            SizedBox(height: 10),
             Text(
-              "Thank You!",
+              "Order Placed Successfully",
               style: TextStyle(
-                color: orange,
-                fontStyle: FontStyle.italic,
+                color: Colors.black,
                 fontFamily: 'SofiaPro',
-                fontSize: 30,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            AnimatedBuilder(
+              animation: _animationController,
+              builder: (context, child) {
+                return ScaleTransition(
+                  scale: _scaleAnimation.drive(
+                    CurveTween(curve: Curves.easeInOutBack),
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: orange,
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.done_rounded,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            SlideTransition(
+              position: _slideAnimation,
+              child: Text(
+                "Thank You!",
+                style: TextStyle(
+                  color: orange,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'SofiaPro',
+                  fontSize: 30,
+                ),
               ),
             ),
           ].separate(10),
