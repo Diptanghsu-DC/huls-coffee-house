447097b19ef894c9d5c9c4c26f00b0dc0cdb841f
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-01-18 20:41:57 +0530

    check duplicate fn modified

--- lib/controllers/services/product/functions/_check_duplicate_impl.dart
+++ lib/controllers/services/product/functions/_check_duplicate_impl.dart
@@ -1,17 +1,47 @@
 part of '../product_controller.dart';
 
-Future<void> _checkDuplicateImpl(ProductModel product) async {
+Future<bool> _checkDuplicateImpl(ProductModel product) async {
+  print("entered check duplicate");
   CollectionReference<Map<String, dynamic>> collection =
       FirebaseFirestore.instance.collection(ProductController._collectionName);
+
+  bool isDuplicate = false;
 
   Query query = collection;
   query = query
       .where(ProductFields.itemName.name, isEqualTo: product.itemName)
-      .where(ProductFields.price.name, isEqualTo: product.price);
+      .where(ProductFields.category.name, isEqualTo: product.category);
 
   QuerySnapshot querySnapshot = await query.get();
 
+  print("query created : $query");
+
   if (querySnapshot.docs.isNotEmpty) {
-    throw Exception("Same item exists, please check inventory");
+    // throw Exception("Same item exists, please check inventory");
+    final documentId = querySnapshot.docs.first.id;
+
+    final document = collection.doc(documentId);
+
+    print("document created");
+
+    Map<String, dynamic> newData = {
+      "itemName": product.itemName,
+      "itemDesc": product.itemDesc,
+      "category": product.category,
+      "price": product.price,
+      "quantity": product.quantity,
+      "ratings": product.ratings,
+      "imageURL": product.imageURL,
+      "lastLocalUpdate": product.lastLocalUpdate,
+    };
+
+    print("new data created");
+
+    document.update(newData);
+
+    print("document updated");
+
+    isDuplicate = true;
   }
+  return isDuplicate;
 }
