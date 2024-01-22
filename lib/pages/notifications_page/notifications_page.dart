1b8516200f998227455296acee75e18ac6912e68
Arunaditya Dasgupta <adg8503@gmail.com>
2024-01-22 21:20:01 +0530

    notification page added to bottom nav

--- lib/pages/notifications_page/notifications_page.dart
+++ lib/pages/notifications_page/notifications_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:huls_coffee_house/pages/login_ui/widgets/buttons.dart';
 
+import '../../widgets/custom_bottom_navigation_bar/custom_bottom_navigation.dart';
+
 class NotificationsPage extends StatefulWidget {
   static const String routeName = '/NotificationsPage';
 
@@ -12,9 +14,21 @@ class NotificationsPage extends StatefulWidget {
 }
 
 class _NotificationsPageState extends State<NotificationsPage> {
+  int _currentIndex = 3;
+
+  void bottomNavigator(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: CustomBottomNavigation(
+        currentIndex: _currentIndex,
+        onTap: bottomNavigator,
+      ),
       appBar: AppBar(
         leading: const GoBackButton(),
         centerTitle: true,
