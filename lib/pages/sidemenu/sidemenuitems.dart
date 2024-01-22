f6f95921b97858c8c1266f69743a6cbc4bb7b9b5
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-01-23 00:43:32 +0530

    minor changes

--- lib/pages/sidemenu/sidemenuitems.dart
+++ lib/pages/sidemenu/sidemenuitems.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 class SideMenuItem {
   final String title;
   final IconData iconPath1;
-<<<<<<< HEAD
-  final Function onTap;
-=======
->>>>>>> 68168cd7be24cdad6d958886e2f324b389ef39ba
 
   SideMenuItem({
     required this.title,
