576fbd8ce7c3dfeb4af50203fcff0e9c9d74333f
Arunaditya Dasgupta <adg8503@gmail.com>
2024-01-23 00:57:13 +0530

    main page added to admin side

--- lib/pages/admin/main_page/main_page.dart
+++ lib/pages/admin/main_page/main_page.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:huls_coffee_house/pages/admin/inventory/inventory.dart';
 
+import '../../../config/config.dart';
+import '../../../utils/utils.dart';
 import '../../notifications_page/notifications_page.dart';
 import '../../pages.dart';
 import '../../profile/profile_main.dart';
@@ -14,15 +17,69 @@ class AdminMainPage extends StatefulWidget {
 
 class _AdminMainPageState extends State<AdminMainPage> {
   List pages = [
+    const ProfilePage(),
     const Inventory(),
     const OrderPage(),
-    const ProfilePage(),
     const NotificationsPage()
   ];
+
+  int currentIndex = 1;
+
+  void navigate(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[0],
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) async{
+        if (didPop) {
+          return;
+        }
+        final NavigatorState navigator = Navigator.of(context);
+        final bool shouldPop = await showExitWarning(context);
+        if (shouldPop) {
+          navigator.pop();
+        }
+      },
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          selectedItemColor: orange,
+          type: BottomNavigationBarType.fixed,
+          onTap: navigate,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.profile_circled,
+              ),
+              label: "Account",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.inventory,
+              ),
+              label: "Inventory",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.timer,
+              ),
+              label: "Orders",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.bell,
+              ),
+              label: "Notifications",
+            ),
+          ],
+        ),
+        body: pages[currentIndex],
+      ),
     );
   }
 }
