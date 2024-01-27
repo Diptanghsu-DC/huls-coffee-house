23dec6d54891ec5050d62fa47cbfd7b0d547ba69
git-ADG <adg8503@gmail.com>
2024-01-27 20:28:05 +0530

    changes to notification

--- lib/notifications/firebase_messagingapi.dart
+++ lib/notifications/firebase_messagingapi.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:huls_coffee_house/notifications/local_notifications.dart';
+import 'local_notifications.dart';
+
 
 Future<void> handleForegroundMessage(RemoteMessage message) async {
   debugPrint('Received message: ${message.notification?.title}');
   if (message.data['action'] == 'show_notification') {
-    await showNotification(message.notification!.title ?? 'Title', message.notification!.body ?? 'Body');
+    await LocalNotification.sendNotification(0,
+      "Huls Coffee House",
+      "update",
+      "Huls Coffee House",
+      "You have an update",);
   }
 }
 
--- lib/notifications/local_notifications.dart
+++ lib/notifications/local_notifications.dart
@@ -1,25 +1,51 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
+class LocalNotification {
+  static bool didInit = false;
 
-Future<void> showNotification(String title, String body) async {
-  const AndroidNotificationDetails androidPlatformChannelSpecifics =
-      AndroidNotificationDetails(
-    'your channel id',
-    'your channel name',
-    channelDescription: 'your channel description',
-    importance: Importance.max,
-    priority: Priority.high,
-    ticker: 'ticker',
-  );
-  const NotificationDetails platformChannelSpecifics =
-      NotificationDetails(android: androidPlatformChannelSpecifics);
-  await flutterLocalNotificationsPlugin.show(
-    0,
-    title,
-    body,
-    platformChannelSpecifics,
-    payload: 'item x',
-  );
+  const LocalNotification._();
+
+  static Future<void> _handlerPermission() async {
+    if (await Permission.notification.isGranted == false) {
+      await Permission.notification.request();
+    }
+  }
+
+  static Future<void> _init() async {
+    if (didInit) return;
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    didInit = true;
+  }
+
+  static Future<void> sendNotification(int id, String channelID,
+      String channelName, String title, String body) async {
+    await _init();
+    await _handlerPermission();
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      channelID,
+      channelName,
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+    NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin.show(
+      id,
+      title,
+      body,
+      notificationDetails,
+    );
+  }
 }
