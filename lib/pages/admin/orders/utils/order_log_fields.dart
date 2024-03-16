4e478c392ac65cc815156874e1fe9535dadcdda1
Diptanghsu-DC <115690935+Diptanghsu-DC@users.noreply.github.com>
2024-03-17 03:15:56 +0530

    fields for logging order

---
+++ lib/pages/admin/orders/utils/order_log_fields.dart
@@ -0,0 +1,11 @@
+class OrderLogFields {
+  static const String date = 'Date';
+  static const String time = 'Time';
+  static const String orderName = 'Order_name';
+  static const String orderQuantity = 'Order_quantity';
+  static const String totalPrice = 'Total_price';
+  static const String orderCompletedBy = 'Order_completed_by';
+
+  static List<String> getFields() =>
+      [date, time, orderName, orderQuantity, totalPrice, orderCompletedBy];
+}
